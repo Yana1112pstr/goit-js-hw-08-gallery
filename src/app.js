6441c06065ca9fae5f8b40ec168b06e6ed114171
Yana Nechveeva <79226190+Yana1112pstr@users.noreply.github.com>
2021-08-20 16:07:19 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -63,3 +63,109 @@ const galleryItems = [
       description: 'Lighthouse Coast Sea',
     },
   ];
+
+const refs = {
+gallery: document.querySelector('.js-gallery'),
+lightbox: document.querySelector('.js-lightbox'),
+lightboxOverlay: document.querySelector('.lightbox__overlay'),
+lightboxImage: document.querySelector('.lightbox__image'),
+btnClose: document.querySelector('button[data-action="close-lightbox"]'),
+};
+
+const {gallery, lightbox, lightboxOverlay, lightboxImage, btnClose} = refs;
+
+const createGallery = (item) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.setAttribute('href', item.original);
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.setAttribute('src', item.preview);
+  galleryImage.setAttribute('data-source', item.original);
+  galleryImage.setAttribute('alt', item.description);
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+const renderMarkup = galleryItems.map((item) => createGallery(item));
+gallery.append(...renderMarkup);
+
+gallery.addEventListener('click', openModal);
+btnClose.addEventListener('click', closeModal);
+lightboxOverlay.addEventListener('click', onOverlayClick);
+
+  function openModal (event) {
+    event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  lightbox.classList.add('is-open');
+  lightboxImage.src = event.target.getAttribute('data-source');
+  lightboxImage.alt = event.target.alt;
+  window.addEventListener('keydown', onEscPress);
+  gallery.addEventListener('keydown', onArrowRight);
+  gallery.addEventListener('keydown', onArrowLeft);
+  };
+  
+  function closeModal () {
+    lightbox.classList.remove('is-open');
+    lightboxImage.src = '';
+    lightboxImage.alt = '';
+    window.removeEventListener('keydown', onEscPress);
+    gallery.removeEventListener('keydown', onArrowRight);
+    gallery.removeEventListener('keydown', onArrowLeft);
+  };
+
+  function onOverlayClick(event) {
+    if (event.target === event.currentTarget) {
+          closeModal();
+    }
+  };
+
+  function onEscPress(event) {
+   if (event.code === 'Escape') {
+     closeModal();
+   }
+  };
+
+  function onArrowRight(event) {
+    if (event.code === 'ArrowRight') {
+      onRightNext()
+    }
+  };
+function onRightNext() {
+  console.log('стр вправо')
+  const activeImage = galleryItems.findIndex((img) => img.original === lightboxImage.src)
+  let index = activeImage ? activeImage : 0;
+  if (index < galleryItems.length - 1) {
+    index += 1;
+  }
+  else {index = 0}
+  lightboxImage.src = galleryItems[index].original;
+  lightboxImage.alt = galleryItems[index].alt;
+};
+
+function onArrowLeft(event) {
+  if (event.code === 'ArrowLeft') {
+    onLeftNext()
+  }
+};
+function onLeftNext() {
+  console.log('стр влево')
+  const activeImage = galleryItems.findIndex((img) => img.original === lightboxImage.src)
+  let index = activeImage ? activeImage : galleryItems.length - 1;
+  if (index > 0) {
+    index -= 1;
+  }
+  else {index = galleryItems.length - 1}
+  lightboxImage.src = galleryItems[index].original;
+  lightboxImage.alt = galleryItems[index].alt;
+};
+
+
